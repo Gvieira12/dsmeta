0b9d54c6913288d7fe4976a6978af7118e1a576c
Gabriel Vieira <gabriel.vieirasilva0712@gmail.com>
2022-09-08 10:59:10 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+import '../src/components/NotificationButton/styles.css'
 
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
